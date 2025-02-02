3ab2909d318fbdaa5982355d0b69ef9d6437e87e
Tahmid Ziko <tahmdziko@gmail.com>
2025-02-02 20:45:05 +0600

    footer fixed

--- src/Views/layouts/main.php
+++ src/Views/layouts/main.php
@@ -1,23 +1,25 @@
 <?php include __DIR__ . '/header.php'; ?>
 
-<div class="container mt-4">
-    <?php if (isset($_SESSION['success'])): ?>
+<div class="main-content">
+    <div class="container mt-4">
+        <?php if (isset($_SESSION['success'])): ?>
         <div class="alert alert-success alert-dismissible fade show">
             <?= $_SESSION['success'] ?>
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         </div>
         <?php unset($_SESSION['success']); ?>
-    <?php endif; ?>
+        <?php endif; ?>
 
-    <?php if (isset($_SESSION['error'])): ?>
+        <?php if (isset($_SESSION['error'])): ?>
         <div class="alert alert-danger alert-dismissible fade show">
             <?= $_SESSION['error'] ?>
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         </div>
         <?php unset($_SESSION['error']); ?>
-    <?php endif; ?>
+        <?php endif; ?>
 
-    <?= $content ?? '' ?>
+        <?= $content ?? '' ?>
+    </div>
 </div>
 
 <?php include __DIR__ . '/footer.php'; ?>
