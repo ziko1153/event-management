6bec78dcd2cb74e84f45f14659cba88737dc04f2
Tahmid Ziko <tahmdziko@gmail.com>
2025-02-02 16:14:52 +0600

    default timezone added

--- config/Initial.php
+++ config/Initial.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
 });
-
+date_default_timezone_set('Asia/Dhaka');
 require_once __DIR__ . '/../config/Database.php';
 require_once __DIR__ . '/../config/Route.php';
